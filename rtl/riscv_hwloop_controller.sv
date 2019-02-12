c75752fbc231703ea664d7c378535d877b8a6372
otaviocomp <otavio10espsanto@gmail.com>
2019-02-12 16:10:05 -0300

    adding block label to satisfy quartus weird needs

--- rtl/riscv_hwloop_controller.sv
+++ rtl/riscv_hwloop_controller.sv
@@ -56,7 +56,7 @@ module riscv_hwloop_controller
   // generate comparators. check for end address and the loop counter
   genvar i;
   generate
-    for (i = 0; i < N_REGS; i++) begin
+    for (i = 0; i < N_REGS; i++) begin : for_gen_comparators
       always @(*)
       begin
         pc_is_end_addr[i] = 1'b0;
